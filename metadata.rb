25c7562f0ba5977910e649c9b07672411d609849
Jeremy Deininger <jeremydeininger@gmail.com>
2014-04-10 15:37:05 -0700

    adding name of cookbook

--- metadata.rb
+++ metadata.rb
@@ -4,3 +4,4 @@ license          "MIT License"
 description      "installs and configures Twitter Storm"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.2.2"
+name             "storm"
