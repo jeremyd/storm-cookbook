95d14070bc3b08b9fd47901d1c9f5b4c8ea77737
Jeremy Deininger <jeremydeininger@gmail.com>
2014-04-16 16:20:40 -0700

    removing zk install from storm (using separate zk)

--- metadata.rb
+++ metadata.rb
@@ -5,3 +5,5 @@ description      "installs and configures Twitter Storm"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.2.2"
 name             "storm"
+
+depends "lookout-java"
--- recipes/default.rb
+++ recipes/default.rb
@@ -7,25 +7,11 @@
 # All rights reserved - Do Not Redistribute
 #
 
-%w[ curl unzip build-essential pkg-config libtool autoconf git-core uuid-dev python-dev zookeeper ].each do |pkg|
+%w[ curl unzip build-essential pkg-config libtool autoconf git-core uuid-dev python-dev ].each do |pkg|
     package pkg do
         retries 2
         action :install
     end
-end
-
-bash "Setup zookeeper as a daemon" do
-  code <<-EOH
-  sudo ln -s /usr/share/zookeeper/bin/zkServer.sh /etc/init.d/zookeeper
-  EOH
-  not_if do
-    ::File.exists?("/etc/init.d/zookeeper")
-  end
-end
-
-service "zookeeper" do
-  supports :status => true, :restart => true, :reload => true
-  action [ :enable, :start ]
 end
 
 bash "Storm install" do
